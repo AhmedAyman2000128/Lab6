018a3c5b82f7906defc65b8093700c6826c2d42b
Mahmoud Hamdy <mahmoud-mahgoub1@hotmail.com>
2023-04-20 00:57:13 +0200

    add hello

--- Sorting.java
+++ Sorting.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
+//Hello ahmed
 // Mahmoud Hamdy Mohamed Mostafa 2001300
 public class Sorting {
     private static List<Node> sort(NodeList classesNodeList) {
